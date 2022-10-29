01e063cc6983fbe48a711dd8a0cf0f426806f69c
Blake Stoddard <blake.m.stoddard@gmail.com>
2022-10-29 00:53:41 +0000

    uncomment commands in the bootstrap script

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#yarn install
-#yarn tsc
+yarn install
+yarn tsc
 APP_CONFIG_backend_baseUrl="https://${CODESPACE_NAME}-7007.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" \
   yarn dev
